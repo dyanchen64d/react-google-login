1848edd57b273f7b2a5b648ba33fa450875ff32f
chen.zhang <zhangchen64d@gmail.com>
2023-03-04 17:21:33 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from 'react';
+import jwtDecode from 'jwt-decode';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world.</div>;
+  const [user, setUser] = useState(null);
+
+  const handleCredentialResponse = (res) => {
+    const user = jwtDecode(res.credential);
+    console.log('Encoded JWT ID token: ', user);
+    setUser(user);
+    document.getElementById('signIn').hidden = true;
+  };
+
+  const signOut = () => {
+    setUser(null);
+    document.getElementById('signIn').hidden = false;
+  };
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: '', // create project on GCP
+      callback: handleCredentialResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById('signIn'), {
+      theme: 'outline',
+      size: 'large',
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+
+  return (
+    <div className="App">
+      {user && (
+        <div>
+          <img src={user.picture} alt="" />
+          <h3>{user.name}</h3>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      )}
+      <div id="signIn" />
+    </div>
+  );
 }
 
 export default App;
